08e96fbaf3a98a84c2429e014d3fc4a1618b3bc9
Aaryan Rustagi <49600516+rustagiaaryan@users.noreply.github.com>
2024-09-09 01:09:29 -0700

    google auth fix

--- poker-tracker-backend/config/googleAuth.js
+++ poker-tracker-backend/config/googleAuth.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://poker-tracker-backend.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
